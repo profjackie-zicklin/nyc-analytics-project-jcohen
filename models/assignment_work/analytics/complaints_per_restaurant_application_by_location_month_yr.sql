f893762b830f3771aa6b4a747f7c2ef5fadfbf5c
ProfJackie Cohen <jaclyn.cohen@baruch.cuny.edu>
2026-05-05 15:42:21 +0000

    update all clear inner joins

--- models/assignment_work/analytics/complaints_per_restaurant_application_by_location_month_yr.sql
+++ models/assignment_work/analytics/complaints_per_restaurant_application_by_location_month_yr.sql
@@ -19,9 +19,9 @@
           d.month,
           COUNT(*) AS total_complaints
       FROM {{ ref('fact_dot_311_requests') }} f
-      JOIN {{ ref('dim_location') }} l ON f.location_key = l.location_key
-      JOIN {{ ref('dim_date') }} d ON f.created_date_key = d.date_key
-      JOIN {{ ref('dim_complaint_type') }} ct ON f.complaint_type_key = ct.complaint_type_key
+      INNER JOIN {{ ref('dim_location') }} l ON f.location_key = l.location_key
+      INNER JOIN {{ ref('dim_date') }} d ON f.created_date_key = d.date_key
+      INNER JOIN {{ ref('dim_complaint_type') }} ct ON f.complaint_type_key = ct.complaint_type_key
       WHERE ct.complaint_category IN ('Street Issues', 'Sidewalk Issues')
       GROUP BY l.borough, l.zip_code, d.year, d.month
   )
@@ -35,7 +35,7 @@
       c.total_complaints as total_sidewalk_or_street_complaints,
       ROUND(c.total_complaints * 1.0 / r.total_restaurants, 2) AS complaints_per_restaurant
   FROM restaurant_counts r
-  JOIN sidewalk_street_complaint_counts c
+  INNER JOIN sidewalk_street_complaint_counts c
       ON r.borough = c.borough
       AND r.zip_code = c.zip_code
       AND r.year = c.year
--- models/assignment_work/analytics/restaurant_app_complaint_counts_per_zip_code.sql
+++ models/assignment_work/analytics/restaurant_app_complaint_counts_per_zip_code.sql
@@ -3,7 +3,7 @@ WITH restaurant_counts AS (
           l.zip_code,                                                                                                                                                                
           COUNT(*) AS total_restaurants                                                                                                                                              
       FROM {{ ref('fact_restaurant_applications') }} f                                                                                                                               
-      JOIN {{ ref('dim_location') }} l ON f.location_key = l.location_key
+      INNER JOIN {{ ref('dim_location') }} l ON f.location_key = l.location_key
       GROUP BY l.zip_code
   ),
 
@@ -12,9 +12,9 @@ WITH restaurant_counts AS (
           l.zip_code,
           COUNT(*) AS total_complaints
       FROM {{ ref('fact_dot_311_requests') }} f
-      JOIN {{ ref('dim_location') }} l ON f.location_key = l.location_key
-      JOIN {{ ref('dim_date') }} d ON f.created_date_key = d.date_key
-      JOIN {{ ref('dim_complaint_type') }} ct ON f.complaint_type_key = ct.complaint_type_key
+      INNER JOIN {{ ref('dim_location') }} l ON f.location_key = l.location_key
+      INNER JOIN {{ ref('dim_date') }} d ON f.created_date_key = d.date_key
+      INNER JOIN {{ ref('dim_complaint_type') }} ct ON f.complaint_type_key = ct.complaint_type_key
       WHERE ct.complaint_category IN ('Sidewalk Issues', 'Street Issues', 'Bike Issues')
         AND d.year BETWEEN 2020 AND 2024
       GROUP BY l.zip_code
@@ -26,6 +26,6 @@ WITH restaurant_counts AS (
       c.total_complaints AS total_sidewalk_street_bike_complaints,
       ROUND(c.total_complaints * 1.0 / r.total_restaurants, 2) AS complaints_per_restaurant
   FROM restaurant_counts r
-  JOIN complaint_counts c
+  INNER JOIN complaint_counts c
       ON r.zip_code = c.zip_code
   ORDER BY r.zip_code
